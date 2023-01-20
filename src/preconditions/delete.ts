6696a3b8b1661f462c314bd6e3a5b519351cf55b
Greensky <david.hesliere@gmail.com>
2023-01-20 17:47:53 +0100

    refactor: :recycle: Run prettier on delete precondition

--- src/preconditions/delete.ts
+++ src/preconditions/delete.ts
@@ -1,12 +1,11 @@
-import { Precondition } from "amethystjs";
+import { Precondition } from 'amethystjs';
 
-export default new Precondition('deleteMessage')
-.setMessageRun(({ message }) => {
-    message.delete().catch(() => {})
+export default new Precondition('deleteMessage').setMessageRun(({ message }) => {
+    message.delete().catch(() => {});
 
     return {
         ok: true,
         channelMessage: message,
         isChatInput: false
-    }
-})
+    };
+});
